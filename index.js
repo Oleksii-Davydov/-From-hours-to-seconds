4ea14982d0e00980d7fb1e96dd4f0f7e57898f7a
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-31 20:08:06 +0200

    fixes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const time=prompt("Enter the number of hours." )
-result='In '+time+' hours '+3600*(+time)+' seconds'
-alert(result)
+const time = prompt("Enter the number of hours.");
+let result = `In ${time} hours ${3600 * time} seconds`
+if (time === null || isNaN(+time)) {
+    alert("Incorrect data")
+} else {
+    alert(result)
+}
 
 
